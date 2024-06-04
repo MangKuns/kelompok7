0325b3c93ded67cccb58b3a2684b637c2e99b773
unknown <kusumarizky650@gmail.com>
2024-06-04 09:09:53 +0700

    First commit

---
+++ build/classes/Frame/Report_Class.rs
@@ -0,0 +1,1 @@
+report_tubes.Report_Class
---
+++ src/Frame/Report_Tubes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Frame;
+
+/**
+ *
+ * @author shadkinx
+ */
+public class Report_Tubes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new Report_GUI().setVisible(true);
+        }
+    });
+}
+    
+}
